4650304cfc89d7dff22abd62eb4d626f8e67410f
Andrii Tsapko <sandriy972@gmail.com>
2024-10-31 14:16:40 +0200

    add

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import MovieList from "../../components/MovieList/MovieList"
+
+
+const HomePage = () => {
+  return (
+      <>
+        <p>Home</p>
+       <MovieList />   
+    </>
+  )
+}
+
+export default HomePage
